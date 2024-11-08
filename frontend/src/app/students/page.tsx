704d378ee9754a4a2f1404e6211c07a48e65f8c5
Facundo Barrera <barreraaybarfacundo@gmail.com>
2024-11-08 19:10:18 -0300

    Add table to students view

--- frontend/src/app/students/page.tsx
+++ frontend/src/app/students/page.tsx
@@ -1,10 +1,67 @@
+"use client"
+
+import { useEffect, useState } from "react"
 import Header from "../../components/Header"
 import Button from "../../components/shared/button/button"
+import { Cell, HeadCell, Row, Table } from "../../components/table/Table"
+import studentsService from "../../services/students"
 
 const Home: React.FC = () => {
+    const [count, setCount] = useState<number>(0)
+    const [students, setStudents] = useState<Student[]>([])
+    const [tableComponents, setTableComponents] = useState<React.ReactNode[]>([])
+
+    const fetchData = async () => {
+        const serviceResponse = await studentsService.getStudents()
+
+        if (serviceResponse.success) {
+            setCount(serviceResponse.data.count)
+            setStudents(serviceResponse.data.students)
+        }
+        // else {
+        //     En este caso, el único insalvable que puede haber es el Network error, que es manejado en otro lugar
+        // }
+    }
+
+    useEffect(() => {
+        const newTableComponents = [
+            <Row key={-1}>
+                <HeadCell>Legajo</HeadCell>
+                <HeadCell>Nombre</HeadCell>
+                <HeadCell>Apellido</HeadCell>
+                <HeadCell>Acciones</HeadCell>
+            </Row>
+        ]
+        
+        newTableComponents.push(...students.map((student, key) => {
+            return (
+                <Row key={key}>
+                    <Cell>{student.sid}</Cell>
+                    <Cell>{student.firstname}</Cell>
+                    <Cell>{student.lastname}</Cell>
+                    <Cell>
+                        <Button color="customred" href="#" name="Borrar" />
+                    </Cell>
+                </Row>
+            )
+        }))
+
+        setTableComponents(newTableComponents)
+    }, [students])
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+    
     return (
         <div className="flex-1 min-w-fit">
             <Header title="Alumnos" button={<Button color="darkturquoise" href="/addStudent" name="Agregar"></Button>}/>
+
+            <main className="px-5 mt-5">
+                <Table>
+                    {tableComponents}
+                </Table>
+            </main>
         </div>
     )
 }
---
+++ frontend/src/components/table/Table.tsx
@@ -0,0 +1,42 @@
+import Button from "../shared/button/button"
+
+interface TableProps {
+    children: React.ReactNode | React.ReactNode[]
+}
+
+export const Table: React.FC<TableProps> = ({ children }) => {
+    return (
+        <table className="w-full border border-solid border-black border-collapse">
+            <thead>
+                {Array.isArray(children) ? children[0] : children}
+            </thead>
+            <tbody>
+                {Array.isArray(children) ? children.filter((_, index) => index !== 0) : null}
+            </tbody>
+        </table>
+    )
+}
+
+export const Row: React.FC<TableProps> = ({ children }) => {
+    return (
+        <tr className="border border-solid border-black border-collapse">
+            {children}
+        </tr>
+    )
+}
+
+export const HeadCell: React.FC<TableProps> = ({ children }) => {
+    return (
+        <th className="border px-2 py-0.5 border-solid border-black border-collapse font-bold">
+            {children}
+        </th>
+    )
+}
+
+export const Cell: React.FC<TableProps> = ({ children }) => {
+    return (
+        <td className="border px-2 py-1 border-solid border-black border-collapse">
+            {children}
+        </td>
+    )
+}
