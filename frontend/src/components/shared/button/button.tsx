ffb78a729aa0b6f425afa8bad2b48d06cecae98a
Facundo Barrera <barreraaybarfacundo@gmail.com>
2024-11-08 18:52:14 -0300

    Add limitations to button colors

--- frontend/src/components/shared/button/button.tsx
+++ frontend/src/components/shared/button/button.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
 
 interface ButtonProps {
-    color: string;
     name: string;
     href: string;
+    color?: 'darkturquoise' | 'paleturquoise' | 'customred'
 }
 
-const Button: React.FC<ButtonProps> = ({ color, name, href }) => {
-    const buttonClasses = `bg-${color} p-[7px] shadow-[0px_0px_0px_1px_rgba(0,_0,_0,_0.75)] text-white text-center no-underline inline-block cursor-pointer rounded`;
-
+const Button: React.FC<ButtonProps> = ({ name, href, color }) => {
     return (
         <a href={href}>
-            <button className={buttonClasses}>
+            <button className={`bg-${color || 'darkturquoise'} px-[7px] py-1 text-white text-center no-underline inline-block cursor-pointer rounded shadow-[0px_0px_0px_1px_rgba(0,_0,_0,_0.75)]`}>
                 {name}
             </button>
         </a>
