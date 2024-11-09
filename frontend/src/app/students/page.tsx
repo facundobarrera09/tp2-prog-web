042a74953c3a6eaca0651365a8af1018e378ca0f
Facundo Barrera <barreraaybarfacundo@gmail.com>
2024-11-08 21:15:37 -0300

    Add paginator

--- frontend/src/app/students/page.tsx
+++ frontend/src/app/students/page.tsx
@@ -6,15 +6,22 @@ import Button from "../../components/shared/button/button"
 import { Cell, HeadCell, Row, Table } from "../../components/shared/table/Table"
 import studentsService from "../../services/students"
 import SearchBar from "../../components/shared/table/SearchBar"
+import Paginator from "../../components/shared/table/Paginator"
 
 const Home: React.FC = () => {
     const [count, setCount] = useState<number>(0)
-    const [searchCriteria, setSearchCriteria] = useState("")
     const [students, setStudents] = useState<Student[]>([])
     const [tableComponents, setTableComponents] = useState<React.ReactNode[]>([])
 
+    const [searchCriteria, setSearchCriteria] = useState("")
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const [pageSize, setPageSize] = useState(5)
+
     const fetchData = async () => {
-        const serviceResponse = await studentsService.getStudents(searchCriteria)
+        // console.log('search', searchCriteria, ', page', currentPage, ', pageSize', pageSize)
+
+        const serviceResponse = await studentsService.getStudents(searchCriteria, currentPage, pageSize)
 
         if (serviceResponse.success) {
             setCount(serviceResponse.data.count)
@@ -23,6 +30,24 @@ const Home: React.FC = () => {
         // else {
         //     En este caso, el único insalvable que puede haber es el Network error, que es manejado en otro lugar
         // }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [searchCriteria, pageSize, currentPage])
+
+    const handlePageSizeChange = (value: number) => {
+        setPageSize(value)
+        setCurrentPage(1)
+    }
+
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page)
+    }
+
+    const handleSearchChange = (value: string) => {
+        setCurrentPage(1)
+        setSearchCriteria(value)
     }
 
     useEffect(() => {
@@ -53,21 +78,21 @@ const Home: React.FC = () => {
 
     useEffect(() => {
         fetchData()
-    }, [searchCriteria])
-
-    useEffect(() => {
-        fetchData()
     }, [])
     
     return (
         <div className="flex-1 min-w-fit">
             <Header title="Alumnos" button={<Button color="darkturquoise" href="/addStudent" name="Agregar"></Button>}/>
 
-            <main className="px-5 mt-5">
-                <SearchBar setSearchCriteria={(value) => { setSearchCriteria(value) }} />
+            <main className="px-5 my-5">
+                <SearchBar setSearchCriteria={handleSearchChange} />
                 <Table>
                     {tableComponents}
                 </Table>
+                <Paginator 
+                    page={currentPage} pageSize={pageSize} count={count}
+                    setPage={handlePageChange} setPageSize={handlePageSizeChange} 
+                />
             </main>
         </div>
     )
---
+++ frontend/src/components/shared/table/Paginator.tsx
@@ -0,0 +1,117 @@
+"use client"
+
+import { ReactNode, useEffect, useState } from "react"
+
+interface PaginatorProps {
+    count: number
+    page: number
+    pageSize: number
+    
+    setPageSize(value: number): void
+    setPage(page: number): void
+}
+
+const Paginator: React.FC<PaginatorProps> = ({ count, page, pageSize, setPageSize, setPage }) => {
+    const [buttonComponents, setButtonComponents] = useState<ReactNode[]>([])
+
+    const paginatorPreviousPage = () => {
+        if (page > 1) {
+            setPage(page - 1)
+        }
+    }
+    function paginatorNextPage() {
+        if (page < Math.ceil(count/pageSize)) {
+            setPage(page + 1)
+        }
+    }
+
+    const updateButtons = () => {
+        const newButtonComponents = []
+
+        newButtonComponents.push(
+            <button
+                key={-1}
+                className="w-7 h-7  bg-slate-200 text-center enabled:cursor-pointer disabled:text-slate-400"
+                disabled={page <= 1}
+                onClick={paginatorPreviousPage}
+            >
+                {'<'}
+            </button>
+        )
+        
+        const range = 2
+        const pages = count/pageSize !== 0 ? Math.ceil(count/pageSize) : 1
+
+        const lowerLimit = (page - range <= 0 || (-2*range + pages) <= 1) ? 1 :
+                 (page + range <= pages) ? (page - range) :
+                 (-2*range + pages)
+    
+        const upperLimit = (page + range >= pages || (2*range + 1) >= pages) ? pages :
+                    (page - range > 0) ? (page + range) :
+                    (2*range + 1)
+
+        for (let x = lowerLimit; x <= upperLimit; x++) {
+            newButtonComponents.push(
+                <button 
+                    key={x}
+                    className={(x !== page ?
+                        'w-7 h-7  bg-slate-200 text-center cursor-pointer' :
+                        'w-7 h-7  bg-lime-500 text-white text-center cursor-pointer'
+                    )}
+                    onClick={() =>{
+                        if (x !== page) {
+                            setPage(x)
+                        }
+                    }}
+                >
+                    {x}
+                </button>
+            )
+        }
+
+        newButtonComponents.push(
+            <button
+                key={upperLimit + 1}
+                className="w-7 h-7 bg-slate-200 text-center enabled:cursor-pointer disabled:text-slate-400"
+                disabled={page >= pages}
+                onClick={paginatorNextPage}
+            >
+                {'>'}
+            </button>
+        )
+
+        setButtonComponents(newButtonComponents)
+    }
+
+    useEffect(() => {
+        updateButtons()
+    }, [page, pageSize, count])
+
+    useEffect(() => {
+        updateButtons()
+    }, [])
+
+    return (
+        <div className="w-full mt-2 flex justify-end">
+            <form className="w-fit flex">
+                <label htmlFor="items-per-page" className="mr-1">Ítems por página: </label>
+                <select
+                    id="items-per-page"
+                    className="px-1 py-0.5 border border-solid border-slate-500 rounded-sm"
+                    onChange={(e) => { setPageSize(Number(e.currentTarget.value)) }}
+                >
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="15">15</option>
+                    <option value="20">20</option>
+                </select>
+            </form>
+
+            <div className="flex ml-2" id="students-paginator">
+                {buttonComponents}
+            </div>
+        </div>
+    )
+}
+
+export default Paginator
