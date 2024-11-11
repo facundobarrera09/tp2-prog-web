fcf64524dbaf7f6516cdcaeb2c24317418a62cfc
Facundo Barrera <barreraaybarfacundo@gmail.com>
2024-11-11 17:02:28 -0300

    Fix color bug in button

--- frontend/src/components/shared/button/button.tsx
+++ frontend/src/components/shared/button/button.tsx
@@ -3,13 +3,15 @@ import React, { HTMLAttributes } from 'react';
 interface ButtonProps {
     name: string;
     href: string;
-    color?: 'darkturquoise' | 'paleturquoise' | 'customred'
+    color?: 'darkturquoise' | 'red' | 'customred'
 }
 
 const Button: React.FC<ButtonProps> = ({ name, href, color }) => {
     return (
         <a href={href}>
-            <button className={`bg-${color || 'darkturquoise'} px-[7px] py-1 text-white text-center no-underline inline-block cursor-pointer rounded shadow-[0px_0px_0px_1px_rgba(0,_0,_0,_0.75)]`}>
+            {/* Los nombres de las clases deben estar completos para ser interpretados por tailwind
+            No se puede hacer `bg-{color}` */}
+            <button className={`${color === 'darkturquoise' ? 'bg-darkturquoise' : 'bg-customred'} px-[7px] py-1 text-white text-center no-underline inline-block cursor-pointer rounded shadow-[0px_0px_0px_1px_rgba(0,_0,_0,_0.75)]`}>
                 {name}
             </button>
         </a>
