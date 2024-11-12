1aba889994243d5bcfd9abc721c4ba8f2b3b5cd7
Facundo Barrera <barreraaybarfacundo@gmail.com>
2024-11-12 00:53:00 -0300

    Add href to 'Modulo alumnos' card

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,9 +5,11 @@ const Home: React.FC = () => {
         <div className="flex-1 min-w-fit">
             <Header title="Página Principal" />
             <div className="p-5">
-                <div className="flex justify-center items-center py-5 shadow-card">
-                    <span className="text-4xl font-bold">Módulo Alumnos</span>
-                </div>
+                <a href="/students">
+                    <div className="flex justify-center items-center py-5 shadow-card">
+                        <span className="text-4xl font-bold">Módulo Alumnos</span>
+                    </div>
+                </a>
             </div>
         </div>
     )
